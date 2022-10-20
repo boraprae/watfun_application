4e7b604b0470cf4ee87d0618d40c43b82bc8e8e5
boraprae <72434691+boraprae@users.noreply.github.com>
2022-10-20 18:44:08 +0700

    prepare the server 2

---
+++ server/dbConfig.js
@@ -0,0 +1,8 @@
+const config = {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "watfundb",
+}
+
+module.exports = config;
--- server/server.js
+++ server/server.js
@@ -1,168 +1,175 @@
-//---------- import -----------
-const express = require("express");
-// const bcrypt = require('bcrypt');
+require('dotenv').config();
+const express = require('express');
+// const jwt = require('jsonwebtoken');
+const path = require('path');
 const app = express();
-const path = require("path");
-const upload = require('./uploadConfig.js');
 
-const bcrypt = require("bcrypt");
+// static folders
+app.use('/public', express.static(path.join(__dirname, "/public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //---- mysql -----
 const mysql = require("mysql");
 const config = require("./dbConfig.js");
-const e = require("express");
-const { database, password } = require("./dbConfig.js");
 const con = mysql.createConnection(config);
 
-//middleware is .use
-app.use(express.static(path.join(__dirname, "public"))) //สามารถเข้าถึงได้อย่างอิสระทำให้ไม่จำเป็นต้องระบุโฟลเดอร์หน้าไฟล์ script ถ้าใช้ cdn ไม่จำเป็นต้องใช้ก็ได้
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// ============= Dummy data ================
+const user = [
+    { 'user_id': 1, 'user_username': 'admin', 'user_password': '1111', 'user_role': 1 },
+    { 'user_id': 2, 'user_username': 'user2', 'user_password': '2222', 'user_role': 2 },
+    { 'user_id': 3, 'user_username': 'user3', 'user_password': '3333', 'user_role': 2 },
+]
 
-// ****** Page Routes ******
-// ----- root service -----
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/upload.html"))
-});
-// app.get("/admin", (req, res) => {
-//     res.sendFile(path.join(__dirname, "/admin.html"))
+const data = [
+    { 'id': 1, 'title': 'First', 'detail': 'aaa', 'user_id': 2 },
+    { 'id': 2, 'title': 'Second', 'detail': 'bbb', 'user_id': 2 },
+    { 'id': 3, 'title': 'Third', 'detail': 'ccc', 'user_id': 3 },
+    { 'id': 4, 'title': 'Fourth', 'detail': 'ddd', 'user_id': 3 },
+]
+
+// // ==================== Middleware ==============
+// function checkUser(req, res, next) {
+//     let token = req.headers['authorization'] || req.headers['x-access-token'];
+//     if (token == undefined || token == null) {
+//         // no token
+//         res.status(400).send('No token');
+//     }
+//     else {
+//         // token found
+//         if (req.headers.authorization) {
+//             const tokenString = token.split(' ');
+//             if (tokenString[0] == 'Bearer') {
+//                 token = tokenString[1];
+//             }
+//         }
+//         jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+//             if (err) {
+//                 res.status(400).send('Incorrect token');
+//             }
+//             else {
+//                 req.decoded = decoded;
+//                 // back to the service
+//                 next();
+//             }
+//         });
+//     }
+// }
+
+// // ------------- Routes ---------------------
+// // ************* Page routes **************
+// app.get('/', (req, res) => {
+//     const message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+//     res.render('index', { message: message });
 // });
-app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "/login.html"))
-});
-// app.get("/users", (req, res) => {
-//     res.sendFile(path.join(__dirname, "/views/pagination.html"))
+
+// app.get('/blogs', (_req, res) => {
+//     const data = [
+//         { 'id': 1, 'title': 'First', 'detail': 'aaa' },
+//         { 'id': 2, 'title': 'Second', 'detail': 'bbb' },
+//         { 'id': 3, 'title': 'Third', 'detail': 'ccc' },
+//     ];
+//     res.render('blog', { post: data });
 // });
 
-app.post("/login", (req, res) => {
-        // console.log("test");
-        // const { username, password } = req.body;
-        const username = req.body.username;
-        const password = req.body.password;
-    
-        const sql = `SELECT username, role FROM user WHERE username='${username}' AND password='${password}'`;
-        //    console.log(sql);
-        con.query(sql, function (err, result, field) {
-            if (err) {
-                console.log(err);
-                res.status(500).send("Database server error");
-            } else {
-                if (result[0].role == 1) {
-                    res.send("/users");
-                    //console.log("Success")
-                }
-                else {
-                    res.send("/users");
-                }
+// // ************* Authen routes **************
+// app.post('/login', (req, res) => {
+//     setTimeout(() => {
+//         const { username, password } = req.body;
+//         const result = user.filter((value) => {
+//             return value.user_username == username && value.user_password == password;
+//         });
+//         // res.json(result);
 
-            }
-        })
-    });
+//         if (result.length == 1) {
+//             const payload = { user: username, user_id: result[0].user_id };
+//             const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: '1d' });
+//             res.json({ url: '/blogs', token: token });
+//             // res.send('/blogs');
+//         }
+//         else {
+//             res.status(400).send('Wrong username or password');
+//         }
+//     }, 3000);
+// });
 
-//------ Get all users -----
-app.get("/user", function (req,res){
-    const sql = "SELECT id, username, role FROM user";
-    con.query(sql,function(err,result){
-        if(err){
-            console.log(err);
-            res.status(500).send("Database server error");
-        } else {
-            res.json(result);
-        }
-    });
-});
+// // jwt creation / encode
+// app.get('/jwt', (_req, res) => {
+//     const username = 'admin';
+//     const payload = { user: username };
+//     const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: '1d' });
+//     res.send(token);
+// });
 
-//------- Delete user -------
-app.delete("/user/:id", function(req,res){
-    const id = req.params.id;
-    const sql = "DELETE FROM user WHERE id=?";
-    con.query(sql, [id], function(err,result){
-        if(err){
-            console.log(err);
-            res.status(500).send("Database server error");
-        } else {
-            if(result.affectedRows == 1){
-                res.send("Delete Done");
-            } else {
-                res.status(500).send("Delete error");
-            }
-        }
-    });
-});
+// // jwt decode
+// app.post('/jwtDecode', (req, res) => {
+//     // const token = req.body.token;
+//     // const token = req.headers['x-access-token'];
+//     // const token = req.headers['authorization'];
+//     let token = req.headers['authorization'] || req.headers['x-access-token'];
+//     if (token == undefined || token == null) {
+//         // no token
+//         res.status(400).send('No token');
+//     }
+//     else {
+//         // token found
+//         if (req.headers.authorization) {
+//             const tokenString = token.split(' ');
+//             if (tokenString[0] == 'Bearer') {
+//                 token = tokenString[1];
+//             }
+//         }
+//         jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+//             if (err) {
+//                 res.status(400).send('Incorrect token');
+//             }
+//             else {
+//                 res.send(decoded);
+//             }
+//         });
+//     }
+// });
 
+// app.delete('/post', (req, res) => {
+//     let token = req.headers['authorization'] || req.headers['x-access-token'];
+//     if (token == undefined || token == null) {
+//         // no token
+//         res.status(400).send('No token');
+//     }
+//     else {
+//         // token found
+//         if (req.headers.authorization) {
+//             const tokenString = token.split(' ');
+//             if (tokenString[0] == 'Bearer') {
+//                 token = tokenString[1];
+//             }
+//         }
+//         jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+//             if (err) {
+//                 res.status(400).send('Incorrect token');
+//             }
+//             else {
+//                 // delete the post in DB....
+//                 res.send('Deleted. Thanks ' + decoded.user);
+//             }
+//         });
+//     }
+// });
 
-//------- Add user ------
-app.post("/user/add", function(req,res){
-    const name = req.body.name;
-    const pass = req.body.pass;
-    const role = req.body.role;
-    
-    const sql = `INSERT TO user VALUE (NULL, username, password, role)`;
+// // ----------- Mobile routes ------------
+// app.get('/mobile/blogs', checkUser, (req, res) => {
+//     // get userID from token
+//     const result = req.decoded;
+//     const userID = result.user_id;
+//     // filter blog posts for this user
+//     const posts = data.filter((value) => {
+//         return value.user_id == userID;
+//     });
+//     res.json(posts);
+// });
 
-    database.query(sql, [username, password, role], function(err, result){
-        if (err) {
-            console.log(err);
-            res.status(500).send("Database server error.");
-        } else {
-            if (result.affectedRows == 1) {
-                res.send("New user has been added.");
-            } else {
-                res.status(501).send("Error while adding new user.");
-            }
-        }
-    });
-});
-
-//---- Upload file
-app.post("/uploading", function(req,res){
-    upload(req,res, function(err){
-        if(err){
-            console.log(err);
-            res.status(500).send("Upload error");
-        }else{
-            res.send("Upload done!");
-        }
-    })
-});
-
-//--- Register -----
-app.post("/register", function(req,res){
-    upload(req,res, function(err){
-        if(err){
-            console.log(err);
-            res.status(500).send("Upload error");
-        }else{
-            //upload done to DB
-            const username = req.body.username;
-            const password = req.body.password;
-            const filename = req.file.filename;
-
-            //hash password
-            bcrypt.hash(password, 10, function(err,hash){
-                if(err){
-                    console.log(err);
-                    res.status(500).send("Hash error");
-                }else{
-                    //get hashed password
-                    
-                    //insert to DB
-                    const sql = "INSERT INTO user (username, password, role, image) VALUES(?,?,2,?)";
-                    con.query(sql, [username, hash, filename], function(err, result){
-                        if(err){
-                            console.log(err);
-                            res.status(500).send("DB error");
-                        }else{
-                            res.send("done");
-                        }
-                    });
-                }
-            })
-        }
-    });
-});
-
-//--start server--
-const PORT = 5000;
-app.listen(PORT, (req, res) => {
-    console.log("Server is running at " + PORT);
+// ---------- Sever starts here ---------
+const PORT = 6000;
+app.listen(PORT, () => {
+    console.log('Server is running at ' + PORT);
 });
